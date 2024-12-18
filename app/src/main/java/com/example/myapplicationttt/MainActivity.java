a7e6dd5c8be76abccbebd00b86a5810bc3b7983f
ma7mos <mahmodw212@gmail.com>
2024-12-18 13:19:44 +0200

    gfjhg

--- app/src/main/java/com/example/myapplicationttt/MainActivity.java
+++ app/src/main/java/com/example/myapplicationttt/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
                 R.drawable.im5
         };
 
-        private int currentIndex = 0;
+        private int currentInde = 0;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -36,14 +36,14 @@ public class MainActivity extends AppCompatActivity {
 
 
             nextButton.setOnClickListener(v -> {
-                currentIndex = (currentIndex + 1) % images.length;
-                imageView.setImageResource(images[currentIndex]);
+                currentInde = (currentInde + 1) % images.length;
+                imageView.setImageResource(images[currentInde]);
             });
 
 
             prevButton.setOnClickListener(v -> {
-                currentIndex = (currentIndex - 1 + images.length) % images.length;
-                imageView.setImageResource(images[currentIndex]);
+                currentInde = (currentInde - 1 + images.length) % images.length;
+                imageView.setImageResource(images[currentInde]);
 
             });
 
